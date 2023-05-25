e07589651d23ea39d0ff91ab84a7368c1c4de974
sjahn <asj214@naver.com>
2023-05-25 13:22:35 +0900

    swagger

--- src/comment/dto/comment.dto.ts
+++ src/comment/dto/comment.dto.ts
@@ -1,20 +1,41 @@
-import { PartialType } from '@nestjs/swagger';
-import { IsNumber, IsOptional, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
 import { Transform } from 'class-transformer';
+import { IsNumber, IsOptional, IsNotEmpty } from 'class-validator';
 
 export class SearchCommentDto {
+  @ApiProperty({
+    example: 'post',
+    description: 'commentableType',
+    required: true,
+  })
   @IsNotEmpty()
   commentableType: string;
 
+  @ApiProperty({
+    example: 1,
+    description: 'commentableId',
+    required: true,
+  })
   @IsNumber()
   @Transform(({ value }) => parseInt(value))
   commentableId: number;
   
+  @ApiProperty({
+    example: 10,
+    description: 'take',
+    required: true,
+  })
   @IsNumber()
   @IsOptional()
   @Transform(({ value }) => parseInt(value))
   take: number = 10;
 
+  @ApiProperty({
+    example: 0,
+    description: 'skip',
+    required: true,
+  })
   @IsNumber()
   @IsOptional()
   @Transform(({ value }) => parseInt(value))
@@ -22,12 +43,27 @@ export class SearchCommentDto {
 }
 
 export class CommentDto {
+  @ApiProperty({
+    example: 'post',
+    description: 'commentableType',
+    required: true,
+  })
   @IsNotEmpty()
   commentableType: string;
 
+  @ApiProperty({
+    example: 1,
+    description: 'commentableId',
+    required: true,
+  })
   @IsNumber()
   commentableId: number;
 
+  @ApiProperty({
+    example: 'This is Comment Content',
+    description: '본문',
+    required: true,
+  })
   @IsNotEmpty()
   body: string;
 }
--- src/post/dto/post.dto.ts
+++ src/post/dto/post.dto.ts
@@ -1,10 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 
 export class PostDto {
+  @ApiProperty({
+    example: 'This is Title',
+    description: '제목',
+    required: true,
+  })
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    example: 'This is Post Content',
+    description: '본문',
+    required: true,
+  })
   @IsNotEmpty()
   body: string;
 }
